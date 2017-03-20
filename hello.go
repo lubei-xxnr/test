99b774feb2fc109716632e1dfa26ade9410cef18
lubei <lubei@fenbi.com>
2017-03-20 17:16:15 +0800

    ADD: string reverse util

--- hello.go
+++ hello.go
@@ -1,5 +1,10 @@
-package main
-import "fmt"
+package  main
+
+import ("fmt"
+	"github.com/lubei-xxnr/test/stringutil"
+)
+
 func main(){
 	fmt.Printf("hello world.\n")
+	fmt.Printf(stringutil.Reverse("\n!og, esrever"))
 }
